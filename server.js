55f244c89dee523eb740e6980862368bd5c26183
SkullTrool <edwardgorjuns@gmail.com>
2024-02-23 15:15:32 +0200

    Added h1 tags

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ io.on('connection', socket => {
 
 
 
-const PORT = 3000 || process.env.PORT
+const PORT = 80 || process.env.PORT
 
 server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
